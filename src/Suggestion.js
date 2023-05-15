d0719397b997780dfd7869a0898d064fa2d9b96f
Rishabh <kumarrishabh7631@gmail.com>
2023-05-16 00:21:57 +0530

    rishabh

--- src/Suggestion.js
+++ src/Suggestion.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
 
@@ -11,45 +11,82 @@ const options = [
 function Suggestion() {
   const [inputValue, setInputValue] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [isShow, setIsShow] = useState(false)
+
+  const myref = useRef()
+  const data=[
+    {
+      _id:1,
+      title:'Lorem sdfa dfsa fdsa dfsa'
+    },
+    {
+      _id:2,
+      title:'ram ge dsf dsfa dsa dfsa'
+    },
+    {
+      _id:3,
+      title:'qsa fdsa fdas fsa fdsa dfsa'
+    },
+    {
+      _id:4,
+      title:'fsaeff fsa fs dfsa fdsa dfsa'
+    },
+    {
+      _id:5,
+      title:'dfsa fdsa dfsa'
+    }
+  ]
 
   useEffect(() => {
-
-    const fetchSuggestions = async () => {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${inputValue}`);
-      const data = await response.json();
-      setSuggestions(data);
+    const handleDocumentClick = (event) => {
+      if (
+        myref.current &&
+        !myref.current.contains(event.target)
+      ) {
+        // Clicked outside the inner div, call onBlur function
+        inputBlur();
+      }
     };
 
-    if (inputValue.length > 0) {
-      fetchSuggestions();
-    } else {
-      setSuggestions([]);
-    }
-  }, [inputValue]);
+    document.addEventListener('click', handleDocumentClick);
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, []);
 
 
   const handleSelectOption = (value) => {
     setInputValue(value?.title);
-    setSuggestions([]);
+    setSuggestions([])
   };
 
-
+  const inputFocus = () =>{
+    setIsShow(true)
+    setSuggestions(data)
+  }
+  const inputBlur = (e) =>{
+    setIsShow(false)
+    setSuggestions([])
+  }
+  
   return (
-    <div style={{width:300, alignContent:'center'}}>
+    <div style={{width:300, alignContent:'center'}} ref={myref}>
       <Form.Control
         type="text"
         placeholder="Type something..."
+        onFocus={inputFocus}
+        // onBlur={inputBlur}
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      {suggestions.length > 0 && (
+      {isShow && (
         <ListGroup className="suggestions-list">
           {suggestions.slice(0, 5).map((option) => (
             <ListGroup.Item key={option.value} onClick={() => handleSelectOption(option)}>
               {option.title}
             </ListGroup.Item>
           ))}
-        
         </ListGroup>
       )}
     </div>
